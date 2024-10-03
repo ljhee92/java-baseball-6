99f8b3d4ad6d71974e985e6e7add81a5057423bd
ljhee92 <weilainib7@gmail.com>
2024-10-03 15:04:23 +0900

    Refactor: 비교 메서드 리팩토링

--- src/main/java/model/Comparator.java
+++ src/main/java/model/Comparator.java
@@ -8,36 +8,30 @@ public class Comparator {
     public String compareNumber(List<Integer> computerNumbers, List<Integer> userNumbers) {
         int strike = 0;
         int ball = 0;
+        StringBuilder compareResult = new StringBuilder();
 
         for (int i = 0; i < userNumbers.size(); i++) {
+            if (Objects.equals(userNumbers.get(i), computerNumbers.get(i))) {
+                strike++;
+                continue;
+            } // end if
+
             if (computerNumbers.contains(userNumbers.get(i))) {
                 ball++;
             } // end if
-
-            if (Objects.equals(userNumbers.get(i), computerNumbers.get(i))) {
-                strike++;
-            } // end if
         } // end for
 
-        if (strike == 0 && ball == 1) {
-            return "1볼";
-        } else if (strike == 0 && ball == 2) {
-            return "2볼";
-        } else if (strike == 0 && ball == 3) {
-            return "3볼";
-        } else if (strike == 1 && ball == 2) {
-            return "1볼 1스트라이크";
-        } else if (strike == 1 && ball == 3) {
-            return "2볼 1스트라이크";
-        } else if (strike == 1 && ball == 1) {
-            return "1스트라이크";
-        } else if (strike == 2 && ball == 2) {
-            return "2스트라이크";
-        } else if (strike == 3 && ball == 3) {
-            return "3스트라이크";
-        } // end if
+        if (strike == 0 && ball == 0) {
+            compareResult.append("낫싱");
+        } else if (strike == 0 && ball > 0) {
+            compareResult.append(ball).append("볼");
+        } else if (strike > 0 && ball > 0) {
+            compareResult.append(ball).append("볼 ").append(strike).append("스트라이크");
+        } else {
+            compareResult.append(strike).append("스트라이크");
+        } // end else
 
-        return "낫싱";
+        return compareResult.toString();
     } // compareNumber
 
 } // class
